a10405f629dfb6d7aacea7afe63c91e43aa10238
Tim Schwichtenberg <tschwichtenberg@gmail.com>
2022-11-08 15:48:48 -0600

    users api routes complete

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
-module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/products', require('./products'))
+router.use('orders', require('./orders'))
+
+router.get('/', (req, res, next) => {
+  res.send('All routes in here start with /api')
+})
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
   next(error)
 })
+
+module.exports = router
