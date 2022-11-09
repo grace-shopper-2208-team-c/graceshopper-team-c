d421e92cd574e102085e8c1bbfb67e6f3ede9ca9
Diego RC <diego.rcsd@gmail.com>
2022-11-09 10:43:20 -0700

    mui card test

--- client/features/products/Products.js
+++ client/features/products/Products.js
@@ -4,11 +4,17 @@ import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { allProducts, fetchProducts } from './productsSlice';
 import { NavLink } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const Products = () => {
   const dispatch = useDispatch();
   const products = useSelector(allProducts);
-  // console.log(products);
+  console.log(products);
 
   useEffect(() => {
     dispatch(fetchProducts());
@@ -24,13 +30,32 @@ const Products = () => {
                 key={`All Products ${product.id}`}
               > */}
               <div className="product row">
-                <p>{product.name}</p>
-                <p>{product.description}</p>
-                <p>{product.category}</p>
-                <p>{product.price}</p>
-                <p>{product.quantity}</p>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    alt="shoe picture"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {product.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Shoe Type: {product.category}
+                      <br />
+                      Description: {product.description}
+                      <br />
+                      Price: ${product.price}
+                      <br />
+                      {product.quantity} in stock
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Add to cart</Button>
+                  </CardActions>
+                </Card>
               </div>
-              {/* </NavLink> */}
             </div>
           ))
         : null}
