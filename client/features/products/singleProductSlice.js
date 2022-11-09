b808eadfbfc546fee637cf58b2b101075ff4259f
Diego RC <diego.rcsd@gmail.com>
2022-11-09 15:38:57 -0700

    fixed single view func

--- client/features/products/singleProductSlice.js
+++ client/features/products/singleProductSlice.js
@@ -1,32 +1,35 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const fetchProductByIdAsync = createAsyncThunk('singleProduct', async (id) => {
-  try {
-    const { data } = await axios.get(`/api/products/${id}`);
-    return data;
-  } catch (err) {
-    console.log(err);
-  };
-});
+export const fetchProductByIdAsync = createAsyncThunk(
+  'singleProduct',
+  async (id) => {
+    try {
+      const { data } = await axios.get(`/api/products/${id}`);
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
 
 const singleProductSlice = createSlice({
-    name: 'singleProduct',
-    initialState: {
-      singleProduct: {},
-      loading: false,
-    },
-    reducers: {},
-    extraReducers: (builder) => {
-      builder.addCase(fetchProductByIdAsync.fulfilled, (state, action) => {
-        console.log("Action payload is ", action.payload)
-        state.singleProduct = action.payload; //likely culprit
-      });
-    },
+  name: 'single_product',
+  initialState: {
+    singleProduct: {},
+    loading: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchProductByIdAsync.fulfilled, (state, action) => {
+      console.log('Action payload is ', action.payload);
+      return action.payload;
+    });
+  },
 });
 
 export const showSingleProduct = (state) => {
-    return state.singleProduct;
+  return state.single_product;
 };
-    
+
 export default singleProductSlice.reducer;
