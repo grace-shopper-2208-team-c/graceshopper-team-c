a67bfadc0cfd43fdbfe3bb72834aefd644738922
Dillon Castelluccio <dilloncas@hotmail.com>
2022-11-09 12:23:40 -0500

    update

--- client/features/users/Users.js
+++ client/features/users/Users.js
@@ -3,14 +3,11 @@ import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { allUsers, fetchUsers } from './usersSlice';
-import { NavLink } from 'react-router-dom';
 
 const AllUsers = () => {
     const dispatch = useDispatch();
     const users = useSelector(allUsers);
     // console.log(users);
-
-    const Navigate = useNavigate();
 
     useEffect(() => {
         dispatch(fetchUsers());
@@ -25,12 +22,10 @@ const AllUsers = () => {
         <>
         <h2>Users</h2>
 
-        <div className='all-users'>
+        <div className='users'>
             {users.map((user) => {
                 return (<div key={user.id} className='user'>
-                    <Link key={user.id} to={ `/users/${user.id}`}>
                         <h3>{user.name}</h3>
-                    </Link>
                         <h3>{user.street_address}</h3>
                         <h3>{user.city}</h3>
                         <h3>{user.state}</h3>
