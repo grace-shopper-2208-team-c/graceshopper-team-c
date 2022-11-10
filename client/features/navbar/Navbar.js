2d141168ae38c761cd04a11e81b2818234129f46
Diego RC <diego.rcsd@gmail.com>
2022-11-10 13:22:23 -0700

    user settings component and route set up complete

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../../app/store';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
@@ -12,37 +13,43 @@ const Navbar = () => {
     dispatch(logout());
     navigate('/login');
   };
-  <h1><Link to="/">Grace HOPS</Link></h1>
+  <h1>
+    <Link to="/">Grace HOPS</Link>
+  </h1>;
 
   return (
     <>
-    <div className='topnav'>
-    <h1><Link to="/">Grace HOPS</Link></h1>
+      <div className="topnav">
+        <h1>
+          <Link to="/">Grace HOPS</Link>
+        </h1>
 
-      <nav>
-
-        {isLoggedIn ? (
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <button type="button" onClick={logoutAndRedirectHome}>
-              Logout
-            </button>
-            <Link to="/cart"><ShoppingCartIcon /></Link>
-          </div>
-        ) : (
-          <div className='authbuttons'>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-            <Link to="/cart">Cart</Link>
-
-          </div>
-        )}
-      </nav>
-    </div>
-    <hr />
-
+        <nav>
+          {isLoggedIn ? (
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/home">Home</Link>
+              <button type="button" onClick={logoutAndRedirectHome}>
+                Logout
+              </button>
+              <Link to="/cart">
+                <ShoppingCartIcon />
+              </Link>
+              <Link to="/settings">
+                <SettingsIcon />
+              </Link>
+            </div>
+          ) : (
+            <div className="authbuttons">
+              {/* The navbar will show these links before you log in */}
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+              <Link to="/cart">Cart</Link>
+            </div>
+          )}
+        </nav>
+      </div>
+      <hr />
     </>
   );
 };
