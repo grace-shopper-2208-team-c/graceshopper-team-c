76f4e38fba7b7ed7dd8105807a955adef0648de4
Diego RC <diego.rcsd@gmail.com>
2022-11-10 10:08:02 -0700

    very light style changes and single view edit

--- client/app/App.js
+++ client/app/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import Navbar from '../features/navbar/Navbar';
 import AppRoutes from './AppRoutes';
-import Products from '../features/products/Products';
-import SingleProduct from '../features/products/SingleProduct';
 
 const App = () => {
   return (
