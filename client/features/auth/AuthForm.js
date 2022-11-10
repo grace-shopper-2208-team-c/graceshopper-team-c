6b8b4872066ea2b6e72c3c16fb962505c4f50c43
Tim Schwichtenberg <tschwichtenberg@gmail.com>
2022-11-10 14:36:36 -0600

    NavBar style updated

--- client/features/auth/AuthForm.js
+++ client/features/auth/AuthForm.js
@@ -4,6 +4,8 @@ import { authenticate } from '../../app/store';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import LockIcon from '@mui/icons-material/Lock';
+
 
 /**
   The AuthForm component can be used for Login or Sign Up.
@@ -25,42 +27,45 @@ const AuthForm = ({ name, displayName }) => {
 
   return (
     <>
-    <Box
-sx={{
-  display: 'flex',
-  flexWrap: 'wrap',
-  justifyContent: "center",
-  alignItems: "center",
-  '& > :not(style)': {
-    m: 1,
-    width: 200,
-    padding: 3,
-  },
-}}
->
-<Paper elevation={3}>
-  <h2 align='center'>Login</h2>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: "center",
+          alignItems: "center",
+          '& > :not(style)': {
+            m: 1,
+            width: 200,
+            padding: 3,
+          },
+        }}
+      >
+        <Paper elevation={3}>
+          <div align="center">
+          <h2><LockIcon /> Login</h2>
+          <form onSubmit={handleSubmit} name={name}>
+            <div>
 
-          <input name="username" placeholder="Username" type="text" required/>
-        </div>
-        <div>
+              <input name="username" placeholder="Username" type="text" required />
+            </div>
+            <div>
 
-          <input name="password" placeholder="Password" type="password" required/>
-        </div>
-        <div>
-        <Button 
-        variant="contained" 
-        type="submit"
-        fullWidth>{displayName}</Button>
-        </div>
-        {error && <div> {error} </div>}
-      </form>
-          </Paper>
-          </Box>
-    
-        </>
+              <input name="password" placeholder="Password" type="password" required />
+            </div>
+            <div>
+              <Button
+                type="submit"
+                sx={{ width: 150, padding: 1 }}
+                variant="contained" 
+              >{displayName}</Button>
+            </div>
+            {error && <div> {error} </div>}
+          </form>
+          </div>
+        </Paper>
+      </Box>
+
+    </>
   );
 };
 
--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../../app/store';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
@@ -12,37 +12,38 @@ const Navbar = () => {
     dispatch(logout());
     navigate('/login');
   };
-  <h1><Link to="/">Grace HOPS</Link></h1>
 
   return (
     <>
     <div className='topnav'>
-    <h1><Link to="/">Grace HOPS</Link></h1>
 
       <nav>
 
         {isLoggedIn ? (
           <div>
-            {/* The navbar will show these links after you log in */}
+            <div className = "lefttopnav"><h1><Link to="/">Grace HOPS</Link></h1></div>
+              <div className = "righttopnav">
             <Link to="/home">Home</Link>
             <button type="button" onClick={logoutAndRedirectHome}>
               Logout
             </button>
-            <Link to="/cart"><ShoppingCartIcon /></Link>
+            <Link to="/cart"><ShoppingCartOutlinedIcon /></Link>
+            </div>
           </div>
         ) : (
-          <div className='authbuttons'>
-            {/* The navbar will show these links before you log in */}
+          <div>
+            <div className = "lefttopnav"><h1><Link to="/">Grace HOPS</Link></h1></div>
+              <div className = "righttopnav">
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
-            <Link to="/cart">Cart</Link>
+            <Link to="/cart"><ShoppingCartOutlinedIcon /></Link>
 
+          </div>
           </div>
         )}
       </nav>
     </div>
-    <hr />
-
+<hr></hr>
     </>
   );
 };
