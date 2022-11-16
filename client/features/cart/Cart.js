be3edd7f91bc643d8d0bb73c49c53bafcb0131c4
Harry <harry.huang887@gmail.com>
2022-11-16 10:56:18 -0500

    handing over cart to tim

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector} from 'react-redux';
 // add your logged in user cart here
 import GuestCart from './GuestCart';
+import loggedCart from './loggedCart';
 
 const Cart = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
@@ -9,9 +10,7 @@ const Cart = () => {
   if (isLoggedIn) {
     //return logged in user cart below
     return (
-      <>
-
-      </>
+      <loggedCart></loggedCart>
     )
   }
   else if (!isLoggedIn) {
--- client/features/cart/loggedCart.js
+++ client/features/cart/loggedCart.js
@@ -1,44 +1,34 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCartByUserIdAsync, fetchOrderProductsByOrderIdAsync } from './cartSlice';
 
-const userId = useSelector((state) => state.auth.me.id)
-
 const loggedCart = () => {
     const dispatch = useDispatch();
-    const isLoggedIn = useSelector((state) => !!state.auth.me.id);
-  
-    if (isLoggedIn) { //logic for logged in user
-      const userId = useSelector((state) => state.auth.me.id);
-  
-      useEffect(() => {
+    const userId = useSelector((state) => state.auth.me.id);
+
+    useEffect(() => {
         dispatch(fetchCartByUserIdAsync(userId));
         dispatch(fetchOrderProductsByOrderIdAsync(cartOrderId)) //array of objects
-      }, [dispatch])
-  
-      const order = useSelector((state) =>
+    }, [dispatch])
+
+    const order = useSelector((state) =>
         state.user_cart.order)
-      const cartOrderId = order.id; //id for the carted order
-  
-      const orderProducts = useSelector((state) => state.user_cart.orderProducts)
-  
-      console.log("orderProducts is ", orderProducts)
-  
-      // const product = useSelector((state) => state.user_cart.product)
-  
-      // console.log('Product', product)
-  
-  
-  
-      //added template to show products in cart
-      return (
+    const cartOrderId = order.id; //id for the carted order
+
+    const products = [];
+    for(product of orderProducts){}
+
+    const orderProducts = useSelector((state) => state.user_cart.orderProducts)
+
+    console.log("orderProducts is ", orderProducts)
+
+    return (
         <div className="cart__left">
-          <div>
-            <h3>My Cart</h3>
-  
-          </div>
+            <div>
+                <h3>My Cart</h3>
+
+            </div>
         </div>
-      );
-  
-    }
+    );
+
 }
